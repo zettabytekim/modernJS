009e640b9cdb58e9c3820deb8260c8d903c470c2
Zetta Kim <zettabyte.kim@gmail.com>
2019-11-21 16:40:40 +0900

    11/21 commit

---
+++ ch08/8-8.js
@@ -0,0 +1,15 @@
+function Person(name, age) {
+    var _name = name;
+    var _age = age;
+    return {
+        getName: function() {return _name;},
+        getAge: function() {return _age;},
+        setAge: function(x) {_age = x;}
+    };
+}
+
+var person = Person("Tom", 18);
+console.log(person.getName());
+console.log(person.getAge());
+person.setAge(19);
+console.log(person.getAge());
